942b24bbffbb2a16a5eb70cf94201d1a2d52b1ea
UT-99 <utkarshtiwari922@gmail.com>
2022-12-13 21:59:23 +0530

    JS - Functions first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+// Q. 1
+
+function withNoParameter(){
+    console.log("This is the function withot any parameter");  
+}
+
+withNoParameter()
+
+//  Q. 2
+
+function addTwoValues (a , b ){
+    console.log(a+b);
+}
+
+addTwoValues(3,56)
+//Q. 3
+
+// Example of Arrow function 
+
+
+let arrowfunction = () => {
+    console.log('This is an arrow function example');
+}
+
+arrowfunction();
+
+
+// Q.4 
+
+
+var x = 21;
+var girl = function () {
+    console.log(x);
+    var x = 20;
+};
+girl ();
+
+
+// Q.5 
+
+
+var x = 21;
+girl ();
+console.log(x)
+function girl() {
+    console.log(x);
+    var x = 20;
+};
+
+
+
+// Q.6 
+
+
+
+var x = 21;
+a();
+b();
+
+  function a() {
+    
+   x = 20;
+  console.log(x);
+};
+ function b() {
+    
+    x = 40;
+   console.log(x);
+};
+
+
+// Q.7 
+
+console.log("This is for factorial: ");
+
+fact = 0 ;
+function Factorial (n){
+    if (n === 1){
+        fact =  1;
+    }
+    else {
+
+        fact = n ;
+
+      
+        for ( let i = n ; i >1 ; i--){
+             fact = fact * (i-1);
+        }
+    }
+    return fact;
+}
+
+console.log(Factorial(5));
